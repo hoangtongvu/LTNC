97cf6bbbdf172d40abd1a2f6cca377b5205f70b3
hoangtongvu <93781995+hoangtongvu@users.noreply.github.com>
2023-03-13 10:00:15 +0700

    Add files via upload

---
+++ bt_hang_tuan/BT08/1.a.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <string.h>
+#include <string>
+#include "Test.h"
+#include <vector>
+
+using namespace std;
+
+
+
+void DeQuy(string s, char res[], int sLength, int count)
+{
+
+	for (int i = 0; i < sLength; i++)
+	{
+		if (count < sLength)
+		{
+			res[count] = s[i];
+			DeQuy(s, res, sLength, count + 1);
+		}
+		else
+		{
+			bool check = true;
+			for (int i = 0; i < sLength - 1; i++)
+			{
+				for (int j = i + 1; j < sLength; j++)
+				{
+					if (res[i] == res[j])
+					{
+						check = false;
+					}
+				}
+			}
+
+			if (check)
+			{
+				for (int i = 0; i < sLength; i++)
+				{
+					cout << res[i];
+				}
+				cout << endl;
+			}
+		}
+	}
+}
+
+
+void reverse(char a[])
+{
+	int length = 0;
+	for (int i = 0; a[i] != '\0'; i++)
+	{
+		length++;
+	}
+	for (int i = 0; i < length / 2; i++)
+	{
+		char temp = a[i];
+		a[i] = a[length - i - 1];
+		a[length - i - 1] = temp;
+	}
+
+}
+
+
+
+
+int main()
+{
+	char a[] = "abcasd";
+	int length = sizeof(a) / sizeof(a[0]);
+	cout << a << endl;
+
+	reverse(a);
+	cout << a << endl;
+
+
+	return 0;
+
+}
+
+
+
---
+++ bt_hang_tuan/BT08/1.b.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <string.h>
+#include <string>
+#include "Test.h"
+#include <vector>
+
+using namespace std;
+
+
+
+void DeQuy(string s, char res[], int sLength, int count)
+{
+	
+	for (int i = 0; i < sLength; i++)
+	{
+		if (count < sLength)
+		{
+			res[count] = s[i];
+			DeQuy(s, res, sLength, count + 1);
+		}
+		else
+		{
+			bool check = true;
+			for (int i = 0; i < sLength - 1; i++)
+			{
+				for (int j = i + 1; j < sLength; j++)
+				{
+					if (res[i] == res[j])
+					{
+						check = false;
+					}
+				}
+			}
+
+			if (check)
+			{
+				for (int i = 0; i < sLength; i++)
+				{
+					cout << res[i];
+				}
+				cout << endl;
+			}
+		}
+	}
+}
+
+
+void delete_char(char* a, char c)
+{
+	int length = 0;
+	for (int i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] != c)
+		{
+			length++;
+		}
+	}
+	char* b = new char[length + 1];
+	int count = 0;
+	for (int i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] != c)
+		{
+			*(b + count) = a[i];
+			count++;
+		}
+	}
+	
+	for (int i = 0; i < count; i++)
+	{
+		*(a + i) = *(b + i);
+	}
+	a[count] = '\0';
+	delete[] b;
+	
+}
+
+
+
+
+int main()
+{
+	char a[] = "abcasd";
+	char c = 'a';
+	int length = sizeof(a) / sizeof(a[0]);
+	cout << a << endl;
+
+	delete_char(a, c);
+	cout << a << endl;
+
+
+	return 0;
+
+}
+
+
+
